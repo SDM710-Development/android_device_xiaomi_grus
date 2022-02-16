3362d1edf010c867a92ac3a549c84cd7f6877ecd
Alex Damaratski <alexeydomoratsky1@gmail.com>
2022-02-16 21:06:28 +0000

    grus: Remove duplicated permission
     * Since https://github.com/SDM710-Development/android_device_xiaomi_sdm710-common/commit/5d849c634ad8405a4a84d9e82acbe097589d94aa
       we have it in common.
    
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: I0101b557583f1242c784f9e842bc4381fc6faddd

--- device.mk
+++ device.mk
@@ -33,7 +33,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
